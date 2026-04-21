92ed62b59c925ff6a7a743326bf18d3370fa37a3
DESKTOP-UA0O1ET\geeth <geethaprasanna422@gmail.com>
2026-04-21 12:28:59 +0530

    Add project files.

---
+++ MyWebApplication1/Pagelifecycle.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Pagelifecycle.aspx.cs" Inherits="MyWebApplication1.Pagelifecycle" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblpreint" runat="server"></asp:Label>
+            <asp:label ID="lblinit" runat="server"></asp:label>
+             <asp:label ID="lblinitcomplete" runat="server"></asp:label>
+             <asp:label ID="lblpageload" runat="server"></asp:label>
+             <asp:label ID="lblpageloadcomplete" runat="server"></asp:label><br />
+
+            <asp:TextBox ID="txtuname" runat="server" OnTextChanged="txtuname_TextChanged" ></asp:TextBox>
+            <asp:Button ID="btnsubmit" runat="server" Text="Submit" OnClick="btnsubmit_Click" /><br />
+
+            <asp:RadioButton ID="rbtelugu" runat="server"  GroupName="language" AutoPostBack="true" />Telugu
+            <asp:Label ID="lbltelugu" runat="server"></asp:Label>
+            <asp:RadioButton ID="rbhindi" runat="server"  GroupName="language" AutoPostBack="true" />Hindi
+            <asp:Label ID="lblhindi" runat="server" ></asp:Label>
+             <asp:RadioButton ID="rbenglish" runat="server"  GroupName="language" AutoPostBack="true" />English
+              <asp:Label ID="lblenglish" runat="server" ></asp:Label><br />
+
+            <asp:CheckBox ID="chkchess" runat="server" AutoPostBack="true" OnCheckedChanged="chkchess_CheckedChanged" />Chess
+            <asp:CheckBox ID="chktennis" runat="server" AutoPostBack="true" OnCheckedChanged="chkchess_CheckedChanged" />Tennis
+            <asp:CheckBox ID="chkcricket" runat="server" AutoPostBack="true" OnCheckedChanged="chkchess_CheckedChanged" />Cricket
+            <asp:Label ID="lblchkmsg" runat="server"></asp:Label>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyWebApplication1/Pagelifecycle.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebApplication1
+{
+    public partial class Pagelifecycle : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblpageload.Text = "HI Gita, Page Load Event<br>";
+            btnsubmit.Visible=false;
+        }
+        protected void Page_PreInIT(object sender, EventArgs e)
+        {
+            lblpreint.Text = "HI Gita, Page PreInit Event <br>";
+        }
+        protected void Page_InIT(object sender, EventArgs e)
+        {
+            lblinit.Text = "HI Gita, Page Init Event <br>";
+        }
+        protected void Page_InITComplete(object sender, EventArgs e)
+        {
+            lblinitcomplete.Text = "HI Gita, Page InitComplete Event <br>";
+        }
+        protected void Page_LoadComplete(object sender, EventArgs e)
+        {
+            lblpageloadcomplete.Text = "HI Gita, Page LoadComplete Event <br>";
+        }
+
+        protected void txtuname_TextChanged(object sender, EventArgs e)
+        {
+            if(txtuname.Text.Length>3)
+                btnsubmit.Visible=true;
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            if (rbtelugu.Checked)
+                lbltelugu.Text = "Selectde Language is Telugu";
+            else if (rbhindi.Checked)
+                lblhindi.Text = "Selected Language is English";
+            else if (rbenglish.Checked)
+                lblenglish.Text = "Selected Language is English";
+        }
+
+        protected void chkchess_CheckedChanged(object sender, EventArgs e)
+        {
+            string check = "";
+            if (chkchess.Checked)
+                check += "Chess,";
+             if (chktennis.Checked)
+                check += "Tennis, ";
+             if (chkcricket.Checked)
+                check += "Cricket.";
+
+            lblchkmsg.Text = "You are Selected : "+check;
+        }
+
+    }
+}
