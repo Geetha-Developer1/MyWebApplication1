c7f02edcd172eeee50c681f170280815482885f3
DESKTOP-UA0O1ET\geeth <geethaprasanna422@gmail.com>
2026-04-21 15:36:40 +0530

    commit

---
+++ MyWebApplication1/ViewState.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ViewState.aspx.cs" Inherits="MyWebApplication1.ViewState" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblmsg" runat="server"></asp:Label><br />
+            <asp:Button ID="btnsubmit" runat="server" Text="Submit" OnClick="btnsubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyWebApplication1/ViewState.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebApplication1
+{
+    public partial class ViewState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(!IsPostBack)
+            {
+                ViewState["Clickscount"] = 0;
+            }
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            //int count = 0;
+            //count++;
+            //lblmsg.Text= "Clicks count: " + count;
+            int count = (int)ViewState["Clickscount"];
+            count++;
+            ViewState["Clickscount"] = count;
+            lblmsg.Text = "Clicks count: " + count;
+        }
+    }
+}
