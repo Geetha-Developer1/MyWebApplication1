7362a71ab0a226ebd41f18f96d856ce7c1e9a6d9
DESKTOP-UA0O1ET\geeth <geethaprasanna422@gmail.com>
2026-04-23 10:57:22 +0530

    c

---
+++ MyWebApplication1/Sessionwithcount.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Sessionwithcount.aspx.cs" Inherits="MyWebApplication1.Sessionwithcount" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblcount" runat="server">  </asp:Label>
+            <asp:Button ID="btnsubmit" runat="server" Text="Submit" OnClick="btnsubmit_Click" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ MyWebApplication1/Sessionwithcount.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace MyWebApplication1
+{
+    public partial class Sessionwithcount : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Clickscount"]==null)
+            {
+                Session["Clickscount"] = 0;
+            }
+            lblcount.Text="Clicks count" + Session["Clickscount"].ToString();
+        }
+
+        protected void btnsubmit_Click(object sender, EventArgs e)
+        {
+            int count = (int)Session["Clickscount"];
+            count++;
+            Session["Clickscount"] = count;
+            lblcount.Text = "Clicks count" + Session["Clickscount"].ToString();
+        }
+    }
+}
